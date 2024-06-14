55cb337cdd74f3fa15d0e2ec4fbf2f78522d9889
sorozco <sergiorozco.ochoa@gmail.com>
2024-06-14 09:23:02 -0500

    Extra layers and news styles

--- src/stores/BoardStore.ts
+++ src/stores/BoardStore.ts
@@ -14,7 +14,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 const boardData: Ref<Board | null> = vueRef(null);
 
