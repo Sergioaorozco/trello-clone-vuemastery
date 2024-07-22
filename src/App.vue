54cd08cee5b05b2b9793576bf2de123023ad0ecf
sorozco <sergiorozco.ochoa@gmail.com>
2024-07-22 18:02:22 -0500

    Adding Swapy to drag and Drop

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,30 @@
 <script setup lang="ts">
-import { useBoardStore } from '../src/stores/BoardStore.ts'
-
 import Column from './components/Column.vue';
+import { onUpdated , ref, nextTick } from 'vue';
+import { useBoardStore } from '../src/stores/BoardStore.ts'
+import { createSwapy } from 'swapy';
 
 const boardStore = useBoardStore();
+const swapyContainer = ref()
+
+onUpdated (() => {
+  nextTick(()=>{
+    if (swapyContainer) {
+      createSwapy(swapyContainer.value, {
+        animation: 'dynamic',
+      });
+    }
+  });
+})
 
 </script>
 
 <template>
-<main>
   <div class="bg-stone-900 h-14 mb-3"></div>
-  <section class="flex gap-x-4 px-4">
-    <div v-for="(column, columnIdx) in boardStore.board?.columns">
-        <Column :key="column.id" :columnInfo="column" :columnInd="Number(columnIdx)" />
-    </div>
-  </section>
-</main>
+  <main ref="swapyContainer" class="flex gap-x-4 px-4 swapyContainer">
+    <Column v-for="(column, columnIdx) in boardStore.board?.columns" :key="Column.id" :columnInfo="column"
+      :columnInd="Number(columnIdx)" :data-swapy-slot="column.name" class="cursor-grab select-none bg-black/40 h-fit rounded-lg" />
+  </main>
 </template>
 
 <style scoped>
--- src/components/Column.vue
+++ src/components/Column.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="bg-slate-50 rounded-md px-4 py-3 w-96 text-slate-600">
-    <p class="font-bold">{{ columnInfo.name }}</p>
-    <hr class="my-4">
-    <ul class="flex flex-col gap-y-2">
-      <taskCard v-for="(card) in columnInfo.tasks" :taskId="card.id" :taskInfo="card" :key="card.id" :columnIdx="columnInd"></taskCard>
-      <li @click="addNewTask">
-        <input v-if="isNewTask" @keyup.enter="addTask()" type="text" :autofocus="isInputVisible" @blur="isInputVisible = false" v-model="newTaskName" :class="newTaskValidation.class" class="taskVal rounded-md border py-3 px-3 w-full" :placeholder="newTaskValidation.text">
-        <div v-else class=" text-slate-600 flex items-center gap-x-2 py-3 px-3 rounded-md transition-colors cursor-pointer w-full hover:bg-blue-100"> 
-          <i class="inline size-5"><plusIcon /></i>Create New Task
-        </div>
-      </li>
-    </ul>
+  <div>
+    <div class="bg-slate-50 rounded-md px-4 py-3 w-96 text-slate-600" :data-swapy-item="columnInd">
+      <p class="font-bold">{{ columnInfo.name }}</p>
+      <hr class="my-4">
+      <ul class="flex flex-col gap-y-2">
+        <taskCard v-for="(card) in columnInfo.tasks" :taskId="card.id" :taskInfo="card" :key="card.id" :columnIdx="columnInd"></taskCard>
+        <li @click="addNewTask">
+          <input v-if="isNewTask" @keyup.enter="addTask()" type="text" :autofocus="isInputVisible" @blur="isInputVisible = false" v-model="newTaskName" :class="newTaskValidation.class" class="taskVal rounded-md border py-3 px-3 w-full" :placeholder="newTaskValidation.text">
+          <div v-else class=" text-slate-600 flex items-center gap-x-2 py-3 px-3 rounded-md transition-colors cursor-pointer w-full hover:bg-blue-100"> 
+            <i class="inline size-5"><plusIcon /></i>Create New Task
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -46,7 +48,7 @@ const props = defineProps({
   columnInd: {
     type:Number,
     required: true
-  }
+  },
 })
 
 // Functions
